e70cebcecde69e43aa9035dda9bf206c108cc1da
TanselArif-21 <tanselarif@live.co.uk>
2018-11-18 17:03:06 +0000

    Finalising. Enabling the saving of the results in the timestamp,erraticity format. Adding some unit tests

--- ErraticStepIdentifier/ErraticStepIdentifier/ErraticStepIdentifier.h
+++ ErraticStepIdentifier/ErraticStepIdentifier/ErraticStepIdentifier.h
@@ -1,8 +1,27 @@
 #pragma once
 
+#include <string>
+
+using namespace std;
+
 class ErraticStepIdentifier
 {
 public:
+	class Trajectory;
+
+	static float roundFloat(float f, int places);
 	static int zero_crossing_detector(float x[], int length);
 	static int value_crossing_detector(float x[], int length, float a);
+	static float* splitByDelimiter(string s, char delim);
+	static string* splitByDelimiterString(string s, char delim);
+	static string* getTimeStamp(string s, char delim);
+	static vector<float*> GetCoordsFromFile(string fileName, char delim);
+	static void GetTrajectoryFromFile(Trajectory t, string fileName, char delim);
+	static vector<string*> GetTimeStampsFromFile(string fileName, char delim);
+	static float Erraticity(vector<float*> x);
+	static float Erraticity(vector<float*> x, int sensitivity);
+	static float* GetErraticitiesArray(vector<float*> x, int size);
+	static float* GetErraticitiesArray(vector<float*> x, int sensitivity, int size);
+	static float* GetSortedErraticitiesArray(vector<float*> x, int sensitivity);
+	static void ProduceErraticitiesFile(string fileInput, string fileOutput, int sensitivity, int size);
 };
--- ErraticStepIdentifier/ErraticStepIdentifier/ErraticStepIdentifierTests/UnitTests.cpp
+++ ErraticStepIdentifier/ErraticStepIdentifier/ErraticStepIdentifierTests/UnitTests.cpp
@@ -1,9 +1,14 @@
 #include "stdafx.h"
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <vector>
 #include "CppUnitTest.h"
 #include "../ErraticStepIdentifier.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
 
 namespace ErraticStepIdentifierTestProject
 {
@@ -126,6 +131,117 @@ namespace ErraticStepIdentifierTestProject
 			int count = ErraticStepIdentifier::zero_crossing_detector(arr, length);
 
 			Assert::AreEqual(1, count);
+		}
+
+		TEST_METHOD(TestMethod10)
+		{
+			//Use the first 5 entries of the test file to return the erraticity to 3 d.p.
+
+			vector <float*> lines;
+
+			string fileName = "C:\\Users\\HVAD\\Documents\\MyStuff\\TempErraticStepIdentifier\\ErraticStepIdentifier\\ErraticStepIdentifier\\ErraticStepIdentifier\\ErraticStepIdentifierTests\\TestFiles\\2017-01-20Z14-30-05.steplog";
+
+			lines = ErraticStepIdentifier::GetCoordsFromFile(fileName, ',');
+
+			vector <float*> lines2;
+
+			for (int i = 0; i < 5; i++)
+			{
+				lines2.push_back(lines[i]);
+			}
+
+			float result = ErraticStepIdentifier::Erraticity(lines2);
+
+			Assert::AreEqual(ErraticStepIdentifier::roundFloat(0.36278710f,3), ErraticStepIdentifier::roundFloat(result,3));
+		}
+
+		TEST_METHOD(TestMethod11)
+		{
+			//Use the next 4 entries of the test file to return the erraticity to 3 d.p.
+
+			vector <float*> lines;
+
+			string fileName = "C:\\Users\\HVAD\\Documents\\MyStuff\\TempErraticStepIdentifier\\ErraticStepIdentifier\\ErraticStepIdentifier\\ErraticStepIdentifier\\ErraticStepIdentifierTests\\TestFiles\\2017-01-20Z14-30-05.steplog";
+
+			lines = ErraticStepIdentifier::GetCoordsFromFile(fileName, ',');
+
+			vector <float*> lines2;
+
+			for (int i = 1; i < 5; i++)
+			{
+				lines2.push_back(lines[i]);
+			}
+
+			float result = ErraticStepIdentifier::Erraticity(lines2);
+
+			Assert::AreEqual(ErraticStepIdentifier::roundFloat(0.447f, 3), ErraticStepIdentifier::roundFloat(result, 3));
+		}
+
+		TEST_METHOD(TestMethod12)
+		{
+			//Use the next first 4 entries of the test file to return the erraticity to 3 d.p. The sensitivity is set to 0.
+
+			vector <float*> lines;
+
+			string fileName = "C:\\Users\\HVAD\\Documents\\MyStuff\\TempErraticStepIdentifier\\ErraticStepIdentifier\\ErraticStepIdentifier\\ErraticStepIdentifier\\ErraticStepIdentifierTests\\TestFiles\\2017-01-20Z14-30-05.steplog";
+
+			lines = ErraticStepIdentifier::GetCoordsFromFile(fileName, ',');
+
+			vector <float*> lines2;
+
+			for (int i = 1; i < 5; i++)
+			{
+				lines2.push_back(lines[i]);
+			}
+
+			float result = ErraticStepIdentifier::Erraticity(lines2,0);
+
+			Assert::AreEqual(ErraticStepIdentifier::roundFloat(0.0f, 3), ErraticStepIdentifier::roundFloat(result, 3));
+		}
+
+		TEST_METHOD(TestMethod13)
+		{
+			//Full erraticity test
+
+			vector <float*> lines;
+
+			vector <string*> lines2;
+
+			vector <float*> lines3;
+
+			string fileName = "C:\\Users\\HVAD\\Documents\\MyStuff\\TempErraticStepIdentifier\\ErraticStepIdentifier\\ErraticStepIdentifier\\ErraticStepIdentifier\\ErraticStepIdentifierTests\\TestFiles\\2017-01-20Z14-30-05.steplog";
+			string fileName2 = "C:\\Users\\HVAD\\Documents\\MyStuff\\TempErraticStepIdentifier\\ErraticStepIdentifier\\ErraticStepIdentifier\\ErraticStepIdentifier\\ErraticStepIdentifierTests\\TestFiles\\TestFile1.txt";
+
+
+			lines = ErraticStepIdentifier::GetCoordsFromFile(fileName, ',');
+			lines2 = ErraticStepIdentifier::GetTimeStampsFromFile(fileName, ',');
+
+			float* f = ErraticStepIdentifier::GetErraticitiesArray(lines, 0, 5);
+
+			string line;
+
+			ifstream myfile(fileName2);
+			if (myfile.is_open())
+			{
+				while (getline(myfile, line))
+				{
+					float* arr;
+
+					arr = ErraticStepIdentifier::splitByDelimiter(line, ',');
+
+					lines3.push_back(arr);
+				}
+				myfile.close();
+			}
+			else
+			{
+				cout << "Unable to open file\n";
+			}
+
+			for (int i = 0; i < lines3.size(); i++)
+			{
+				Assert::AreEqual(ErraticStepIdentifier::roundFloat(lines3[i][1],3), ErraticStepIdentifier::roundFloat(f[i],3));
+			}
 		}
 
 	};
